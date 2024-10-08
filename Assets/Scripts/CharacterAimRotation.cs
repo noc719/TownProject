7e9200c3d5352399a53a18e3f73444d9fd8fcac7
gwanyoungkim719 <0000kgy@naver.com>
2024-10-08 15:33:39 +0900

    Add CharacterAim.cs & CatleAssets

---
+++ Assets/Scripts/CharacterAimRotation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CharacterAimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TownCharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<TownCharacterController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+    public void OnAim(Vector2 Direction)
+    {
+        RotateVision(Direction);
+    }
+
+    private void RotateVision(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+}
